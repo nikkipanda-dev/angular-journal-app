60ee2cb915195b26bb0c6e201aa0aa9564dbf7d4
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-03-21 20:35:13 +0800

    Fixes user post rel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
